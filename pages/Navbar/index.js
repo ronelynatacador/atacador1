ee73335207e84750a5b52b0f222d852fe4e67e64
Ronelyn Atacador <ronelynatacador@gmail.com>
2025-08-30 12:09:49 +0800

    first commit

---
+++ pages/Navbar/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Navbar = () => {
+  return (
+    <header class="text-gray-600 body-font">
+  <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+      <Image src = "/sfac.png" alt="Logo" width={400} height={300} />  
+      </a>   
+     <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+      <a class="mr-5 hover:text-gray-900">Home</a>
+      <a class="mr-5 hover:text-gray-900">About SFAC</a>
+      <a class="mr-5 hover:text-gray-900">Contact Us</a>
+      <a class="mr-5 hover:text-gray-900">Department</a>
+      <a class="mr-5 hover:text-gray-900">Student Services</a>
+    </nav>
+    <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
+      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+        <path d="M5 12h14M12 5l7 7-7 7"></path>
+      </svg>
+    </button>
+  </div>
+</header>
+  )
+}
+
+export default Navbar
